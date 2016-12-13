36ce899dc64a4696319fc02c863200122c26bcad
Jack <Jack_Swenson@Baylor.edu>
2016-12-13 17:45:54 -0600

    seg faulting

--- elope.c
+++ elope.c
@@ -2,36 +2,48 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
-FILE *ngtest;
 unsigned int ret = -1;
-char *str;
+char *args = NULL;
+char *command = NULL;
 
 void sigint(int sig){
-    ret = pclose(ngtest);
-    free(str);
+    if(args)
+        free(args);
     exit(0);
 }
 
-int main(int arg, char **argv) {
+int main(int argc, char **argv) {
 
-    str = malloc(sizeof(char) * 100);
     signal(SIGINT, sigint);
 
-    while(ret != 0) {
+    if(argc <= 1){
+        fprintf(stderr, "No arguments\n");
+        return 1; 
+    }
+    command = malloc(sizeof(char) * 100);
+    command = "/bin/sh";
+    strcat(command, argv[1]);
+    printf("command: %s", command);
 
-        ngtest = popen("ng test 2>&1", "r");
-        printf("%i", ngtest);
-
-        while(!feof(ngtest)){
-            fgets(str, 100, ngtest);
-            printf("%s", str);
+    if(argc > 2){
+        args = malloc(sizeof(char) * 100);
+        for(int i = 2; i < argc; i++){
+            strcat(args, argv[i]);
+            strcat(args, " ");
         }
+    }
+     
+    printf("args: %s", args);
 
-        ret = pclose(ngtest);
+    while(execl(argv[1], args) == -1){
+        perror("error: ");
     }
 
-    free(str);
+    if(args)
+        free(args);
 
     return 0;
 }
